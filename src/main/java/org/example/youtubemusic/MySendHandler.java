d2128ce5aec16b7c11f84d12c43cab7329c7fb1d
adi0212 <74491488+adi0212@users.noreply.github.com>
2022-12-12 20:07:54 +0200

    Refactor deleted code that was example

--- src/main/java/org/example/youtubemusic/MySendHandler.java
+++ src/main/java/org/example/youtubemusic/MySendHandler.java
@@ -1,19 +1,33 @@
 package org.example.youtubemusic;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame;
 import net.dv8tion.jda.api.audio.AudioSendHandler;
 import org.jetbrains.annotations.Nullable;
 
 import java.nio.ByteBuffer;
 
 public class MySendHandler implements AudioSendHandler {
+//    private final AudioPlayer audioPlayer;
+    private AudioFrame lastFrame;
+
+//    public MySendHandler(AudioPlayer audioPlayer) {
+//        this.audioPlayer = audioPlayer;
+//    }
+
     @Override
     public boolean canProvide() {
-        return false;
+//        lastFrame = audioPlayer.provide();
+        return lastFrame != null;
     }
 
-    @Nullable
     @Override
     public ByteBuffer provide20MsAudio() {
-        return null;
+        return ByteBuffer.wrap(lastFrame.getData());
+    }
+
+    @Override
+    public boolean isOpus() {
+        return true;
     }
 }
